7f9f1803d1253fe77a6b89b748285b7dc430162c
Niru27405 <niranjanaravikumar2707@gmail.com>
2025-02-19 06:18:40 +0530

    modified

--- Basic_math/recurssion.py
+++ Basic_math/recurssion.py
@@ -7,4 +7,14 @@ def f(cnt):
     f(cnt +1)
     
 n = int(input())
-f(0)
+f(0) 
+# print name 5 times using recursion
+
+def f(i,n):
+    if(i>n):
+        return
+    print("raj")
+    f(i+1,n)
+n = int(input())
+f(14,n)
+

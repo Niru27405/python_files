fc09e4c8e57cf582fcff5017cbca09999a418ca4
Niru27405 <niranjanaravikumar2707@gmail.com>
2025-03-07 22:28:58 +0530

    File modified

---
+++ Basic_math/Hashing.py
@@ -0,0 +1,20 @@
+# given an array and asked to count no of times particular no is repeated
+# Brute force without hashing
+def f(num,n):
+    cnt = 0
+    for i in range(len(n)):
+        if(n[i]==num):
+            cnt += 1
+    return cnt
+n = list(map(int,input().split()))
+num = int(input())
+d =f(num,n)
+print(d)
+
+# Hashing
+from collections import Counter
+n = int(input())
+f = Counter(n)
+num = int(input())
+print(f[num])
+

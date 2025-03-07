696012c9b8570d358e61256beadd8924db56d92e
Niru27405 <niranjanaravikumar2707@gmail.com>
2025-03-08 04:28:52 +0530

    modified

--- leetcode/TwoSum.py
+++ leetcode/TwoSum.py
@@ -8,3 +8,13 @@ for i in range(len(n)):
             continue
         if(n[i]+n[j]==t):
             print(i,j)
+
+# Optimal solution
+
+n = list(map(int,input().split()))
+t = int(input())
+i=0
+for i in range(i+1,len(n)):
+    for j in range(len(n)):
+        if(n[i]+n[j]==t):
+            print(i,j)
